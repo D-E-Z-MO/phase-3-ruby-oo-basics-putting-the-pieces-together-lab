207fb7094a984d61f1253b154e7d37e05219efae
douglassmoss <douglassmoss@icloud.com>
2022-02-01 21:46:09 -0800

    commit tree 6907aeb6903d388a66581fae0555f427ef6d6cd7

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  attr_reader :book, :title, :author, :page_count, :genre
+
+    def initialize(book)
+      title, author, page_count, genre = book
+      @title = title
+      @author = author
+      @page_count = page_count
+      @genre = genre
+    end
+    def author
+      @author = author
+    end
+
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
 
 
